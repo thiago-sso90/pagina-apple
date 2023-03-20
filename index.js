ff0ad7ec46704b0ff67e5a999753a5a1c96e6373
THIAGO DE SOUZA BARBOSA <thiaguinhosouza73@gmail.com>
2023-03-20 17:41:29 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.getElementsByClassName.background = cor
+}
+
+function trocaimagem(imagem){
+    let imgiphone = document.querySelector(".iphone")
+    imgiphone.src = imagem
+}
